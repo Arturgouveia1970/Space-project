660306c694dfcc15c9d8de8a3c035c36aa1cf247
Felix Odete <felix.odette@gmail.com>
2022-06-28 01:16:31 +0300

    Nav bar with profile link

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import NavBar from './components/nav-bar/NavBar';
 import Profile from './components/profile/Profile';
+import Rockets from './components/rockets/Rockets';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Profile />
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/rockets" />} />
+        <Route path="/rockets" element={<Rockets />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/rockets/Rockets.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Rockets = () => (
+  <div>Rockets go here!</div>
+);
+
+export default Rockets;
