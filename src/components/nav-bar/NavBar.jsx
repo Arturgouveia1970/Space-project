ef11046f7f1458e41b9f9853980288c6e2359180
Felix Odete <felix.odette@gmail.com>
2022-06-27 23:31:21 +0300

    Add profile sample text and nav-bar details

---
+++ src/components/nav-bar/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../../assets/planet.png';
+import './NavBar.css';
+
+const NavBar = () => {
+  const links = [
+    {
+      path: 'profile',
+      text: 'My Profile',
+    },
+  ];
+
+  return (
+    <nav className="nav-bar">
+      <img src={logo} alt="Cosmos Logo." width={50} height={50} />
+      <h1>Space Travellers&ampos; Hub</h1>
+      <ul className="nav-bar-links">
+        {links.map((link) => (
+          <li key={link.path}>
+            <NavLink to={link.path}>
+              <span>{link.text.toUpperCase()}</span>
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
