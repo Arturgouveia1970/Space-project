a75b58a0630ea1c76b006842bbfb00af7a3640f7
Arturgouveia1970 <artgouveiag@gmail.com>
2022-06-28 10:02:13 +0100

    added missions to navbar

---
+++ src/components/Missions.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Missions = () => (
+  <div>Missions here</div>
+);
+export default Missions;
--- src/services/Services.js
+++ src/services/Services.js
@@ -1,9 +1,11 @@
 import http from '../http-client';
 
 const getRockets = () => http.get('/rockets');
+const getMissions = () => http.get('/missions');
 
 const SpaceService = {
   getRockets,
+  getMissions,
 };
 
 export default SpaceService;
