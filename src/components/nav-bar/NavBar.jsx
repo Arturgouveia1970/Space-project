15cd744a647c70e72fe9b037cee5310233c033f0
Felix Odete <felix.odette@gmail.com>
2022-06-28 10:08:14 +0300

    Installed prop-type dependencies

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import logo from '../../assets/planet.png';
 import './NavBar.css';
 
 const NavBar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const toggleMenu = () => {
+    setOpenMenu((openMenu) => !openMenu);
+  };
+
   const links = [
     {
       path: 'rockets',
@@ -21,10 +27,21 @@ const NavBar = () => {
         <img src={logo} alt="Cosmos Logo." width={50} height={50} />
         <h1>Space Travellers&apos; Hub</h1>
       </div>
+      {openMenu ? (<AiOutlineClose onClick={() => toggleMenu()} className="menu-icon" />) : (<AiOutlineMenu onClick={() => toggleMenu()} className="menu-icon" />)}
       <ul className="nav-bar-links">
         {links.map((link) => (
           <li key={link.path}>
             <NavLink to={link.path} className="nav-link">
+              <span>{link.text.toUpperCase()}</span>
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+
+      <ul className="mobile-navUl" style={{ transform: openMenu ? 'translateX(0)' : 'translateX(120%)' }}>
+        {links.map((link) => (
+          <li key={link.path}>
+            <NavLink to={link.path} className="nav-link mobile" onClick={() => toggleMenu()}>
               <span>{link.text.toUpperCase()}</span>
             </NavLink>
           </li>
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,16 +1,16 @@
 import SpaceService from '../../services/Services';
 
 const ADD_ALL_ROCKETS = 'spacehub/rockets/ADD_ALL_ROCKETS';
-const RESERVE_ROCKET = 'spacehub/rockets/RESERVE_ROCKET';
+const TOGGLE_RESERVATION = 'spacehub/rockets/TOGGLE_RESERVATION';
 
 export default function rockets(state = [], action = {}) {
   switch (action.type) {
     case ADD_ALL_ROCKETS:
       return action.payload;
-    case RESERVE_ROCKET:
+    case TOGGLE_RESERVATION:
       return state.map((rocket) => {
         if (rocket.id !== action.payload) return rocket;
-        return { ...rocket, reserved: true };
+        return { ...rocket, reserved: !rocket.reserved };
       });
     default:
       return state;
@@ -26,9 +26,10 @@ export const getRockets = async (dispatch, getState) => {
       name: rocket.rocket_name,
       description: rocket.description,
       flickr_images: rocket.flickr_images[0],
+      reserved: false,
     }));
     dispatch({ type: ADD_ALL_ROCKETS, payload: rockets });
   }
 };
 
-export const reserveRocket = (id) => ({ type: RESERVE_ROCKET, payload: id });
+export const reserveRocket = (id) => ({ type: TOGGLE_RESERVATION, payload: id });
