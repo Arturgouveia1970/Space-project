c5f9a5058932ea655cba5d36f3144ae312d9c1c6
Felix Odete <felix.odette@gmail.com>
2022-06-28 10:16:39 +0300

    Load rocket list

--- src/http-client.js
+++ src/http-client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  BASE_URL: 'https://api.spacexdata.com/v3',
+  baseURL: 'https://api.spacexdata.com/v3',
   headers: {
     'Content-type': 'application/json',
   },
