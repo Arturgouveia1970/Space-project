49a15b325f2ee7a7927bfc72baab0098700160d6
Arturgouveia1970 <artgouveiag@gmail.com>
2022-07-01 07:00:44 +0100

    resolved tests problem

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger/src';
+import { logger } from 'redux-logger';
 import rockets from './rockets/rockets';
 import missions from './missions/missions';
 
